c6338391cad696e8541ecf35abc07d8b315ab0cf
Andres Cubillos <mincubill@gmail.com>
2024-01-12 19:53:02 -0300

    -Cliente --Dockerfile

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  preview: {
+    port: 5173,
+    strictPort: true,
+   },
   server: {
     watch: {
       usePolling: true,
