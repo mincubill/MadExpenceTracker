014d7d5db46fabbe3397b13c6420887e4a72e652
Andres Cubillos <mincubill@gmail.com>
2024-01-14 01:14:21 -0300

    Fix envs

--- Client/src/contants/contants.js
+++ Client/src/contants/contants.js
@@ -1,3 +1,3 @@
 const url = import.meta.env.VITE_BASE_URL
 const port = import.meta.env.VITE_BASE_URL_PORT
-export const baseUrl = `${url}:${port}`
+export const baseUrl = port ? `${url}:${port}` : `${url}`
